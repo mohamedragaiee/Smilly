9565adc5040bb83e15e08048191b593a221b7b37
eslam200788 <eslam200788@gmail.com>
2017-05-09 01:22:20 +0200

    new start

---
+++ js/JQuery.js
@@ -0,0 +1,20 @@
+/*global $, alert, console, mixer */
+$(function () {
+   
+    'use strict';
+    
+
+   /* $('header h1').css('margin-top', ($(window).height() - 100 - $(this).height() / 2));*/
+
+   
+    
+    $('header').height($(window).height());
+
+    
+    $('header').height($(window).height());
+    
+    $('nav .bar i').click(function () {
+       
+        $('nav .nav ').toggleClass('visibel');
+    });
+});
