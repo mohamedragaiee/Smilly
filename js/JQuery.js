079930ec5f1e3011951677a750cdf29b787a13da
eslam200788 <eslam200788@gmail.com>
2017-05-13 04:10:04 +0200

    video

--- js/JQuery.js
+++ js/JQuery.js
@@ -13,4 +13,19 @@ $(function () {
        
         $('nav .nav ').toggleClass('visibel');
     });
+    $(".video i").click(function () {
+    //console.log(this);
+        $('.video video').css('display', 'block');
+       
+        $("video").click(function () {
+         //console.log(this); 
+            if (this.paused) {
+                this.play();
+            } else {
+                this.pause();
+                
+            }
+        });
+    });
+  
 });
