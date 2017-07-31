4e62d04e85c32429e8bbba4aa48445e0cbf667e3
eslam200788 <eslam200788@gmail.com>
2017-07-31 17:18:18 +0200

    31/7/2017

--- js/JQuery.js
+++ js/JQuery.js
@@ -1,27 +1,31 @@
 /*global $, alert, console, mixer */
-$(function () {
-   
+$(function() {
+
     'use strict';
-    
+
     $('header .hdr').css('margin-top', ($(window).height() - 484));
-    
-    $(window).width(function () {
-        
+
+    $(window).width(function() {
+
         if ($(window).width() < 768) {
-            
+
             $('header .hdr').css('margin-top', ($(window).height() / 4));
             $('nav .nav').css('width', '100%');
         }
-        
+
     });
-    
+
     $('header').height($(window).height());
-    
-    $('nav .bar i').click(500, function () {
-       
+
+    $('nav .bar i').click(500, function() {
+
         $('nav .nav ').toggleClass('visibel');
     });
-    
-    
-  
+
+    $('.nav li a').click(function() {
+        $('html body').animate({
+            scrollTop: $('#' + $(this).data('value')).offset().top
+        }, 1200);
+    });
+
 });
